9534a69b436120382ae11eab57738afe65ad8108
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-24 17:22:22 -0400

    Added a toolbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,16 @@
 <template>
-  <div><h1 class="blue lighten-5">NAVBAR</h1></div>
+  <nav>
+    <v-toolbar flat app color="blue lighten-2">
+      <v-toolbar-title class="text-uppercase white--text">
+        <span class="font-weight-light">TODO</span>
+        <span>Stuff</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="blue darken-4">
+        <span>Sign out</span><v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
 </template>
 <script>
 export default {};
