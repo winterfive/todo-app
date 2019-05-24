9464d4a44bce7771547039f7a7ffd04eb305698f
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-24 15:13:48 -0400

    Playing w/ colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+      veniam molestias numquam aliquam porro aut fugit nobis sit veritatis
+      officia quos, animi ipsum! Ducimus nihil libero aperiam iure! Sed,
+      asperiores.
+    </p>
+    <p class="pink darken-4 red--text text--lighten-4">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam eaque
+      perspiciatis aliquam voluptatem voluptates? Reiciendis commodi molestiae
+      dolorum rem, animi modi reprehenderit, iusto quos distinctio odio nobis
+      omnis quaerat velit.
+    </p>
+    <p class="cyan accent-4 cyan--text text--lighten-5">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim distinctio
+      eaque veritatis asperiores, dolor aut officia quod? Hic perferendis omnis
+      aliquam repudiandae illo. Reiciendis, cumque corporis! Excepturi similique
+      blanditiis voluptas?
+    </p>
+    <p class="amber green--text text--darken-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam facere
+      quisquam inventore adipisci dolore atque commodi consequatur quibusdam,
+      quas suscipit veritatis, velit, esse aliquid iste quo voluptates numquam
+      fugit non!
+    </p>
   </div>
 </template>
 
