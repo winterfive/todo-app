7dd11c57545b6b285a550064db820f84f3c14b71
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-28 18:02:57 -0400

    Overwriting default theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,10 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: "#96ceb4",
+    success: "#ffeead",
+    info: "#ffcc5c",
+    error: "#ff6f69"
+  }
 })
