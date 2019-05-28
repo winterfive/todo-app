064efee983beb4ea0526b9efa1bac427b857ff6b
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-28 17:16:10 -0400

    Added navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app color="blue lighten-2">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase white--text">
         <span class="font-weight-light">TODO</span>
         <span>Stuff</span>
@@ -10,9 +11,18 @@
         <span>Sign out</span><v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false
+    }
+  }
+};
 </script>
 
