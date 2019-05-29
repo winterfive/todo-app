4b7a74e58680115afe02efe5d121c0e4a4027101
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-29 16:59:21 -0400

    Resolved issue with status color border not showing

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,30 +2,87 @@
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
     <v-container my-5>
-      <v-card flat class="pa-3">
-        <v-layout row wrap>
+      <v-card flat class="pa-3" v-for="project in projects" :key="project.title"
+        ><v-layout row wrap class="pa-3" :class="project.status">
           <v-flex xs12 md6>
             <div class="caption darkgrey--text">Project Title</div>
-            <div>Create a new website</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption darkgrey--text">Person</div>
-            <div>Darwin T. Kitty</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption darkgrey--text">Due by</div>
-            <div>5th June 2020</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class="caption darkgrey--text">Status</div>
-            <div>Ongoing</div>
+            <div class="caption darkgrey--text">status</div>
+            <div>{{ project.status }}</div>
           </v-flex>
-        </v-layout>
-      </v-card>
+        </v-layout></v-card
+      >
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "title01",
+          person: "person01",
+          due: "5th June 2020",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+          title: "title02",
+          person: "person02",
+          due: "24th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+          title: "title03",
+          person: "person03",
+          due: "10th Jan 2021",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+          title: "title04",
+          person: "person04",
+          due: "30th June 2020",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+      ]
+    };
+  }
+};
 </script>
+
+<style lang="scss">
+$status-border-width: 10px;
+$status-border-style: solid;
+
+.complete {
+  border-left: $status-border-width $status-border-style blue;
+}
+
+.ongoing {
+  border-left: $status-border-width $status-border-style purple;
+}
+
+.overdue {
+  border-left: $status-border-width $status-border-style red;
+}
+</style>
+
