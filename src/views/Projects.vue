d37ce0073104d51a6753600987b0a52d9c1673fa
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-29 15:08:17 -0400

    Added several cards using flexbox

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,13 +2,25 @@
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
     <v-container my-5>
-      <v-card flat>
-        <v-layout row wrap></v-layout>
-        <v-flex xs12 md6>
-         <div class="caption darkgrey--text">Project Title</div>
-
-        </v-flex>
-
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption darkgrey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption darkgrey--text">Person</div>
+            <div>Darwin T. Kitty</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption darkgrey--text">Due by</div>
+            <div>5th June 2020</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="caption darkgrey--text">Status</div>
+            <div>Ongoing</div>
+          </v-flex>
+        </v-layout>
       </v-card>
     </v-container>
   </div>
