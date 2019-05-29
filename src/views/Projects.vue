13cad425ee0ad65b01ae8675831d397a4fca9563
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-29 15:00:13 -0400

    Added v-card

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,7 +2,14 @@
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
     <v-container my-5>
-      content
+      <v-card flat>
+        <v-layout row wrap></v-layout>
+        <v-flex xs12 md6>
+         <div class="caption darkgrey--text">Project Title</div>
+
+        </v-flex>
+
+      </v-card>
     </v-container>
   </div>
 </template>
