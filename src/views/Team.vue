cf74a886c96e95d551f16d81aad1d8702a4de0dd
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-30 16:28:24 -0400

    Added border style to avatars

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -6,7 +6,9 @@
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
           <v-card flat class="text-xs-center ma-2">
-            <v-responsive class="pt-4">image here </v-responsive>
+            <v-responsive class="pt-4">
+              <img :src="person.avatar" class="avatar" />
+            </v-responsive>
             <v-card-text>
               <div class="subheading">{{ person.name }}</div>
               <div class="grey--text">{{ person.role }}</div>
@@ -31,12 +33,37 @@ export default {
   data() {
     return {
       team: [
-        { name: "Darwin T. Kitty", role: "Cute Developer" },
-        { name: "Lisa Chung", role: "DevOps" },
-        { name: "Lee Gainer", role: "Front End Developer" },
-        { name: "Sean McBeth", role: "VR/AR Developer" }
+        {
+          name: "Darwin T. Kitty",
+          role: "Cute Developer",
+          avatar: "https://loremflickr.com/240/240/cat?random=1"
+        },
+        {
+          name: "Lisa Chung",
+          role: "DevOps",
+          avatar: "https://loremflickr.com/240/240/cat?random=2"
+        },
+        {
+          name: "Lee Gainer",
+          role: "Front End Developer",
+          avatar: "https://loremflickr.com/240/240/cat?random=3"
+        },
+        {
+          name: "Sean McBeth",
+          role: "VR/AR Developer",
+          avatar: "https://loremflickr.com/240/240/cat?random=4"
+        }
       ]
     };
   }
 };
 </script>
+
+<style lang="scss" scoped>
+img {
+  border: 2px solid grey;
+  border-radius: 50%;
+  height: 10em;
+  width: 10em;
+}
+</style>
