d539d2e8f6fef07f78cdc18fc6b5f044293f2347
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-31 19:01:57 -0400

    Added db connection to MyProjects, removed hardcoded data from files

--- src/views/MyProjects.vue
+++ src/views/MyProjects.vue
@@ -20,60 +20,36 @@
 </template>
 
 <script>
+import db from "@/fb";
+
 export default {
   data() {
     return {
-      projects: [
-        {
-          title: "Kibble and Us",
-          person: "Darwin T. Kitty",
-          due: "5th May 2019",
-          status: "overdue",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        },
-        {
-          title: "Dropdowns everywhere",
-          person: "Lee Gainer",
-          due: "24th Jan 2019",
-          status: "complete",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        },
-        {
-          title: "Being awesome everyday",
-          person: "Lisa Chung",
-          due: "10th Jan 2021",
-          status: "ongoing",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        },
-        {
-          title: "VR: To the future!",
-          person: "Sean McBeth",
-          due: "30th June 2020",
-          status: "new",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        },
-        {
-          title: "Design an app",
-          person: "Lee Gainer",
-          due: "29 Aug 2019",
-          status: "new",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        }
-      ]
+      projects: []
     };
   },
   computed: {
     // Doing this as there's no authentication in this app
     myProjects() {
       return this.projects.filter(project => {
-        return project.person === "Lee Gainer";
+        if (project.person === "Lee Gainer" || project.person === "LG")
+          return project;
       });
     }
+  },
+  created() {
+    db.collection("projects").onSnapshot(res => {
+      const changes = res.docChanges();
+
+      changes.forEach(change => {
+        if (change.type === "added") {
+          this.projects.push({
+            ...change.doc.data(),
+            id: change.doc.id
+          });
+        }
+      });
+    });
   }
 };
 </script>
