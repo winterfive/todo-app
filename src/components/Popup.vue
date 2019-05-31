80dac9713eda39f4df51c2010a0b54bcf34020cc
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-31 11:06:30 -0400

    Added form to popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,11 +5,24 @@
     >
     <v-card>
       <v-card-title><h2>Add a New Project</h2></v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="title" v-model="title"></v-text-field>
+          <v-textarea label="Information" v-model="content"></v-textarea>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: "",
+      content: ""
+    };
+  }
+};
 </script>
 
