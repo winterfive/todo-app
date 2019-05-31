2a123da6a05c80636d445b7337bee534821eb73b
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-31 11:22:27 -0400

    Added click functionality, storing values input by user, and reset of defualt values in form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -7,8 +7,17 @@
       <v-card-title><h2>Add a New Project</h2></v-card-title>
       <v-card-text>
         <v-form class="px-3">
-          <v-text-field label="title" v-model="title"></v-text-field>
-          <v-textarea label="Information" v-model="content"></v-textarea>
+          <v-text-field
+            label="title"
+            v-model="title"
+            prepend-icon="folder"
+          ></v-text-field>
+          <v-textarea
+            label="Information"
+            v-model="content"
+            prepend-icon="edit"
+          ></v-textarea>
+          <v-btn flat class="primary mx-0" @click="submit">Add Project </v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -20,8 +29,21 @@ export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      // Will become props and passed to other views
+      newProjectTitle: "",
+      newProjectContent: ""
     };
+  },
+  methods: {
+    submit() {
+      // eslint-disable-next-line no-console
+      console.log(this.title, this.content);
+      this.newProjectTitle = this.title;
+      this.title = "";
+      this.newProjectContent = this.content;
+      this.content = "";
+    }
   }
 };
 </script>
